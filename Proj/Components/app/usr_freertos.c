70aa553e6f1e5c5ddee3f2a4c03a707c6e3150cb
Jiang Tianhang <1919524828@qq.com>
2026-08-01 10:56:29 +0800

    single change

--- Proj/Components/app/usr_freertos.c
+++ Proj/Components/app/usr_freertos.c
@@ -2,7 +2,7 @@
  * @Author: Jiang Tianhang 1919524828@qq.com
  * @Date: 2026-07-30 18:16:22
  * @LastEditors: Jiang Tianhang 1919524828@qq.com
- * @LastEditTime: 2026-07-31 14:43:44
+ * @LastEditTime: 2026-07-31 17:10:10
  * @FilePath: \code\Proj\Components\app\usr_freertos.c
  * @Description: 
  * 本项目只用于控制THz专用激励源的控制，切勿用于其他用途，否则西安理工大学及开发者不承担任何责任。
